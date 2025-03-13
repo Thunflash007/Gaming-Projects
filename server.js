766ef0b8b03f4f0076094cf146465cfdccb7d487
Thunflash007 <thunflash007@gmail.com>
2025-03-13 11:14:51 +0000

    version 2

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 3001;
+
+let messages = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,6 +24,21 @@ app.post('/admin', (req, res) => {
     } else {
         res.redirect('/');
     }
+});
+
+// Entfernen der Kontakt-Routen
+// app.get('/contact', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'contact.html'));
+// });
+
+// app.post('/contact', (req, res) => {
+//     const { name, email, phone, message } = req.body;
+//     messages.push({ name, email, phone, message });
+//     res.redirect('/');
+// });
+
+app.get('/messages', (req, res) => {
+    res.json(messages);
 });
 
 app.listen(port, () => {
